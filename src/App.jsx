cd57420e00eee861c8e40f96f8c4b02a290c7beb
SyikhaSonOfAdin <syikhasmart@gmail.com>
2024-03-13 13:29:02 +0700

    Initialized Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+import "./css/additional-styles.css";
+import "./css/tailwind-output.css";
+
+function App() { 
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes path="/">
+          
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
